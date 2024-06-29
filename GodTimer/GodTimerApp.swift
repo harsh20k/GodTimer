35dd122c793a43a53c7d370c1aa3ef8285d6b9af
harsh <harsh.pandey77@gmail.com>
2024-06-29 14:22:30 +0530

    Initial Commit

---
+++ GodTimer/GodTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GodTimerApp.swift
+//  GodTimer
+//
+//  Created by harsh  on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GodTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
