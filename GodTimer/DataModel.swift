2d6ddac5381d16e1e7453d418c17281b81ad398b
harsh <harsh.pandey77@gmail.com>
2024-07-09 11:00:49 +0530

    some changes

--- GodTimer/DataModel.swift
+++ GodTimer/DataModel.swift
@@ -8,12 +8,23 @@ class TimeTracker: ObservableObject {
 	
 	func getTimeString(for interval: TimeInterval) -> String {
 		let minutes = Int(interval) / 60
-		let hours = minutes % 60
+		let hours = Int(minutes) / 60
 		let seconds = Int(interval) % 60
 		if(minutes < 60){
 			return String(format: "%02d:%02d", minutes, seconds)
 		} else {
-			return String(format: "%02d:%02d", hours, minutes)
+			return String(format: "%02d:%02d:%02d", hours, minutes%60, seconds)
+		}
+	}
+	
+	func getFixedSizeTimeString(for interval: TimeInterval) -> String {
+		let minutes = Int(interval) / 60
+		let hours = Int(minutes) / 60
+		let seconds = Int(interval) % 60
+		if(minutes < 60){
+			return String(format: "%02d:%02d", minutes, seconds)
+		} else {
+			return String(format: "%02d:%02d", hours, minutes%60)
 		}
 	}
 }
